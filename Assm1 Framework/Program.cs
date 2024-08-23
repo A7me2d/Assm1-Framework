a533ecb2dea4398483c64dfbcb02578f907178dc
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-08-23 14:40:58 +0300

    Add project files.

---
+++ Assm1 Framework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assm1_Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
